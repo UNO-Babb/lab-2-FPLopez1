971a29ca8d7b39983d52bb4b696a668aafae5c28
FPLopez1 <fpinedalopez2@unl.edu>
2025-09-08 01:29:16 +0000

    Done

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,7 @@
-#FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#FutureTime.pyclear
+#Name: Francisco Pineda Lopez
+#Date: 09/07/2025
+#Assignment: Lab 2
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -9,20 +9,31 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 5) % 24
   currentMinute = now.minute
 
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
+  moreHours = int(input("Enter hours to add: "))
+
   #Ask user for minutes
+  moreMins = int(input("Enter minutes to add: "))
+
+  futureMins = (currentMinute + moreMins) % 60
+  extraHour = (currentMinute + moreMins) // 60
+
+  print 
+  
 
   #Calculate the time after the user-supplied time has passed.
+  futureHours = (currentHour + moreHours + extraHour) %24
 
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
+  print(futureHours, ":", futureMins)
 
 
 if __name__ == '__main__':
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,7 +1,7 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Francisco Pineda Lopez
+#Date: 09/07/2025
+#Assignment: Lab #2
 
 #We will need random for this program, import to use this package.
 import random
@@ -11,8 +11,17 @@ def main():
   print("Magic 8 Ball")
   #Prompt the user for their question.
 
-  #Answer question randomly with one of the options from your earlier list.
+answers = ["As I see it, yes.", "Ask again later", "Yes", "No", "It is decidedly so", "Concentrate and ask again", "Outlook Good", "Without a doubt."]
 
+  #Answer question randomly with one of the options from your earlier list.
+length = len(answers)
+r = random.random() * length
+
+r = int(r)
+
+print(r)
+response = answers[r]
+print(response)
 
 if __name__ == '__main__':
   main()
